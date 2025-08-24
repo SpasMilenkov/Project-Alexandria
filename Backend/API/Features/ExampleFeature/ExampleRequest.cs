b9ee7011aba369c656dd809c077d8c49ec561cca
Spas Milenkov <spasmilenkov123@gmail.com>
2025-08-24 15:39:36 +0300

    feat: add example endpoint for testing

---
+++ Backend/API/Features/ExampleFeature/ExampleRequest.cs
@@ -0,0 +1,8 @@
+namespace API.Features.ExampleFeature;
+
+public class ExampleRequest
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public int Age { get; set; }
+}
---
+++ Backend/API/Features/ExampleFeature/ExampleResponse.cs
@@ -0,0 +1,7 @@
+namespace API.Features.ExampleFeature;
+
+public class ExampleResponse
+{
+    public string FullName { get; set; }
+    public bool IsOver18 { get; set; }
+}
