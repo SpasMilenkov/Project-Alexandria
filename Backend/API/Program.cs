804d88dca8685da531e6e17e2b0bc30ba694b1f3
Spas Milenkov <spasmilenkov123@gmail.com>
2025-08-29 19:06:28 +0300

    feat: add swagger with identity framework

--- Backend/API/Program.cs
+++ Backend/API/Program.cs
@@ -1,10 +1,37 @@
+using Data.Context;
 using FastEndpoints;
+using FastEndpoints.Swagger;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Models;
 
 var bld = WebApplication.CreateBuilder();
-bld.Services.AddFastEndpoints();
+
+var connStr = bld.Configuration.GetConnectionString("AlexandriaPostgres");
+
+bld.Services.AddDbContext<AlexandriaDbContext>(opt => opt.UseNpgsql(connStr));
+bld.Services.AddAuthorization();
+    ;// Register Identity (no MapIdentityApi)
+bld.Services.AddIdentity<ApplicationUser, ApplicationRole>(options =>
+    {
+        options.Password.RequireDigit = true;
+        options.Password.RequireUppercase = true;
+        options.Password.RequireNonAlphanumeric = true;
+        options.SignIn.RequireConfirmedEmail = false;
+    })
+    .AddEntityFrameworkStores<AlexandriaDbContext>()
+    .AddDefaultTokenProviders();
+
+bld.Services.AddFastEndpoints().SwaggerDocument();
 bld.Services.AddHealthChecks();
 
 var app = bld.Build();
-app.UseFastEndpoints();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.UseFastEndpoints()
+    .UseSwaggerGen(); // This enables the Swagger UI;
 app.MapHealthChecks("/health");
+
 app.Run();
