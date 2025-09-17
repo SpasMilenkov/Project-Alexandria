179fdb8f8997346a28201324e32e897e586a9e01
Spas Milenkov <spasmilenkov123@gmail.com>
2025-09-17 17:26:01 +0300

    chore: move minio config to infrastructure

---
+++ Backend/Infrastructure/Config/MinioConfig.cs
@@ -0,0 +1,9 @@
+namespace Infrastructure.Config;
+
+public class MinioConfig
+{
+    public string? Endpoint { get; set; }
+    public string? SecretKey { get; set; }
+    public string? AccessKey { get; set; }
+    public string? UploadBucket { get; set; }
+}
