8ca3a7328df3c232ae372f4077a7dd17e9aafcb9
Spas Milenkov <spasmilenkov123@gmail.com>
2025-10-15 13:36:53 +0300

    feat: add method for fetching file with preview in file repo

--- Backend/Common/IFileRepository.cs
+++ Backend/Common/IFileRepository.cs
@@ -9,4 +9,6 @@ public interface IFileRepository : IRepository<File>
     public Task<File> UpdateAsync(File file, CancellationToken ct = default);
 
     public Task<FileSummary?> GetFileNameAndMimeType(Guid fileId, CancellationToken ct = default);
+
+    public Task<File> GetFileWithPreviewAsync(Guid fileId, CancellationToken ct = default);
 }
