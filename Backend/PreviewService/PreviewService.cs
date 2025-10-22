163877258fbf427d8434efa54e4023c0e6d056a8
Spas Milenkov <spasmilenkov123@gmail.com>
2025-10-22 16:36:06 +0300

    feat: add text and archive preview generation

--- Backend/PreviewService/PreviewService.cs
+++ Backend/PreviewService/PreviewService.cs
@@ -6,6 +6,7 @@ using DTO;
 using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Options;
 using Models.Enumerators;
+using PreviewService.Archives;
 
 namespace PreviewService;
 
@@ -14,13 +15,16 @@ public class PreviewService(
     IImagePreviewService imagePreviewService,
     IOptions<MinioConfig> storageConfig,
     IMemoryCache memoryCache,
-    IPublisherService publisherService) : IPreviewService
+    IPublisherService publisherService,
+    ITextPreviewService textPreviewService,
+    IArchivePreviewService archivePreviewService) : IPreviewService
 {
     private static readonly SemaphoreSlim Pool = new(3, 3);
 
     private static readonly Dictionary<Guid, Task<(byte[] data, FileSummary metadata)>> OngoingTasks = new();
     private static readonly SemaphoreSlim TaskDictionaryLock = new(1, 1);
 
+    //TODO: break down this monstrosity into smaller more manageable chunks
     public async Task<FileResultSummary?> GetPreview(Guid fileId, CancellationToken ct)
     {
          if (memoryCache.TryGetValue(fileId, out (byte[] fileBytes, FileSummary metadata) cacheValue))
@@ -76,6 +80,8 @@ public class PreviewService(
                  switch (category)
                  {
                      case FileCategory.Image:
+                         //TODO: Inspect for memory leaks here or excessive downloading from the backend
+                         //TODO: Ideally this whole code block should be moved to one of the workers
                          var previewStream =
                              await imagePreviewService.GenerateImagePreview(fileStream, fileData.MimeType);
                          await storageService.UploadPreview(
@@ -115,14 +121,68 @@ public class PreviewService(
                      case FileCategory.Document:
                      case FileCategory.Spreadsheet:
                      case FileCategory.Presentation:
-                     case FileCategory.Archive:
                      case FileCategory.Pdf:
                          var documentBody = Encoding.UTF8.GetBytes(fileId.ToString());
                          await publisherService.Publish(documentBody, $"document.{fileData.MimeType.Split('/')[1]}");
-                         //TODO: add proper endpoint behavior when this path returns null
                          return null;
+                     case FileCategory.Archive:
+                         var archivePreview = await archivePreviewService.GenerateArchivePreviewAsync(fileStream, fileData.Name, ct);
+                         
+                         var archivePreviewUpload = await storageService.UploadPreview(
+                             storageConfig.Value.PreviewBucket ?? "user-previews",
+                             "previews/" + fileData.Name + ".json",
+                             "application/json", new MemoryStream(archivePreview.data), fileData.Id, -1, 
+                             uploadedBy:"System",
+                             ct: ct);
+                         
+                         await storageService.UpdateFileMetadata(fileId, hasPreview: true, updatedBy: "system", ct: ct);
+
+                         var archivePreviewSummary = new FileSummary(fileData.Name + ".json", "application/json", true, archivePreviewUpload.Url);
+
+                         var archiveCacheOptions = new MemoryCacheEntryOptions
+                         {
+                             Size = Math.Max(1, archivePreview.data.Length / (1024 * 1024)),
+                             SlidingExpiration = TimeSpan.FromMinutes(5)
+                         };
+
+                         var archiveResultTuple = (archivePreview.data, archivePreviewSummary);
+                         memoryCache.Set(fileId, archiveResultTuple, archiveCacheOptions);
+                             
+                         tcs.SetResult(archiveResultTuple);
+
+                         return new FileResultSummary(new MemoryStream(archivePreview.data),
+                             archivePreviewSummary);
+                     
                      case FileCategory.Text:
-                         break;
+                         var textPreview = await textPreviewService.GenerateTextPreviewAsync(fileStream, fileData.Name, 524288, ct); 
+                         
+                         var textPreviewUpload = await storageService.UploadPreview(
+                             storageConfig.Value.PreviewBucket ?? "user-previews",
+                             "previews/" + fileData.Name,
+                             textPreview.mimeType,
+                             new MemoryStream(textPreview.data),
+                             fileData.Id,
+                             uploadedBy: "System",
+                             ct: ct);
+                         
+                         await storageService.UpdateFileMetadata(fileId, hasPreview: true, updatedBy: "system", ct: ct);
+
+                         var textPreviewSummary = new FileSummary(fileData.Name, "application/json", true, textPreviewUpload.Url);
+
+                             var textCacheOptions = new MemoryCacheEntryOptions
+                             {
+                                 Size = Math.Max(1, textPreview.data.Length / (1024 * 1024)),
+                                 SlidingExpiration = TimeSpan.FromMinutes(5)
+                             };
+
+                             var textResultTuple = (textPreview.data, textPreviewSummary);
+                             memoryCache.Set(fileId, textResultTuple, textCacheOptions);
+                             
+                             tcs.SetResult(textResultTuple);
+
+                             return new FileResultSummary(new MemoryStream(textPreview.data),
+                                 new FileSummary(fileData.Name, textPreview.mimeType, true, textPreviewUpload.Url));
+
                      case FileCategory.Audio:
                      case FileCategory.Video:
                          var mediaBody = Encoding.UTF8.GetBytes(fileId.ToString());
@@ -132,9 +192,6 @@ public class PreviewService(
                      default:
                          throw new ArgumentOutOfRangeException();
                  }
-
-                 tcs.SetResult((Array.Empty<byte>(), null));
-                 return null;
              }
              finally
              {
@@ -162,7 +219,6 @@ public class PreviewService(
             return new FileResultSummary(new MemoryStream(cacheValue.fileBytes), cacheValue.metadata);
 
         await TaskDictionaryLock.WaitAsync(ct);
-        Task<(byte[] data, FileSummary metadata)> ongoingTask;
 
         if (OngoingTasks.TryGetValue(fileId, out var existingTask))
         {
