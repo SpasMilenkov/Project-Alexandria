3d1534f3ea0c8dcd5b4ddc0fa20e6f2e2ee12511
Spas Milenkov <spasmilenkov123@gmail.com>
2025-11-01 15:44:43 +0200

    feat: add refresh token configuration

---
+++ Backend/Data/Configurations/RefreshTokenConfiguration.cs
@@ -0,0 +1,63 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Models;
+
+namespace Data.Configurations;
+
+
+public class RefreshTokenConfiguration : IEntityTypeConfiguration<RefreshToken>
+{
+    public void Configure(EntityTypeBuilder<RefreshToken> builder)
+    {
+        builder.HasKey(e => e.Id);
+        
+        builder.Property(e => e.Token)
+            .HasMaxLength(ValidationConstants.StringLengths.ExtraLongString)
+            .HasColumnType($"varchar({ValidationConstants.StringLengths.ExtraLongString})")
+            .IsRequired();
+        
+        builder.Property(e => e.UserId)
+            .IsRequired();
+        
+        builder.Property(e => e.UpdatedBy)
+            .HasMaxLength(ValidationConstants.StringLengths.UserId)
+            .HasColumnType($"varchar({ValidationConstants.StringLengths.UserId})")
+            .IsRequired(false);
+        
+        // DateTime properties
+        builder.Property(e => e.ExpiresAt)
+            .HasColumnType("timestamp with time zone")
+            .IsRequired();
+        
+        builder.Property(e => e.CreatedAt)
+            .HasColumnType("timestamp with time zone")
+            .IsRequired();
+        
+        builder.Property(e => e.UpdatedAt)
+            .HasColumnType("timestamp with time zone")
+            .IsRequired(false);
+        
+        builder.Property(e => e.DeletedAt)
+            .HasColumnType("timestamp with time zone")
+            .IsRequired(false);
+        
+        builder.Property(e => e.RevokedAt)
+            .HasColumnType("timestamp with time zone")
+            .IsRequired(false);
+        
+        builder.Property(e => e.IsRevoked)
+            .IsRequired();
+        // Relations
+        builder.HasOne(e => e.User)
+            .WithMany()
+            .HasForeignKey(e => e.UserId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.HasIndex(e => e.Token);
+        builder.HasIndex(e => e.UserId);
+        builder.HasIndex(e => e.ExpiresAt);
+        builder.HasIndex(e => e.CreatedAt);
+        
+        builder.ToTable("RefreshTokens");
+    }
+}
