6839af656bad2026d284cc85a432015b94b69338
Spas Milenkov <spasmilenkov123@gmail.com>
2025-11-04 16:27:11 +0200

    feat: update file repo interface with new tag search method

--- Backend/Common/IFileRepository.cs
+++ Backend/Common/IFileRepository.cs
@@ -7,8 +7,12 @@ public interface IFileRepository : IRepository<File>
 {
     public Task<File> CreateAsync(File file, CancellationToken ct = default);
     public Task<File> UpdateAsync(File file, CancellationToken ct = default);
-
+    public Task<File?> GetFileWithTagsAsync(Guid fileId, CancellationToken ct = default);
     public Task<FileSummary?> GetFileNameAndMimeType(Guid fileId, CancellationToken ct = default);
 
+    Task<(IEnumerable<File> Files, int TotalCount)> FindFilesByTagsAsync(
+        FileTagSearchQuery query, 
+        CancellationToken ct = default);
+    
     public Task<File> GetFileWithPreviewAsync(Guid fileId, CancellationToken ct = default);
 }
