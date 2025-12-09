7c6327e1ece2127155ef35151d2a402ff05f80f5
Spas Milenkov <spasmilenkov123@gmail.com>
2025-12-09 11:19:37 +0200

    feat: implement file ownership, add directory relation

--- Backend/Models/File.cs
+++ Backend/Models/File.cs
@@ -34,9 +34,13 @@ public class File : IBase
     [StringLength(ValidationConstants.StringLengths.UserId)]
     public string? UpdatedBy { get; set; }
 
-    public virtual ICollection<Tag> Tags { get; set; } = new List<Tag>();
-    public virtual ICollection<SignedUrl> SignedUrls { get; set; } = new List<SignedUrl>();
+    public ICollection<Tag> Tags { get; set; } = new List<Tag>();
+    public ICollection<SignedUrl> SignedUrls { get; set; } = new List<SignedUrl>();
     
     public Guid PreviewId { get; set; }
     public Preview? Preview { get; set; }
+    public ApplicationUser? Owner { get; set; }
+    public Guid OwnerId { get; set; }
+    public Directory? Directory { get; set; }
+    public Guid? DirectoryId { get; set; }
 }
