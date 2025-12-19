9cc3b94dd4cdc74553267e50407db50d0c1d2350
Spas Milenkov <spasmilenkov123@gmail.com>
2025-12-19 14:35:11 +0200

    refactor: update interfaces to use pagination when fetching files and directories

--- Backend/Common/Services/IDirectoryService.cs
+++ Backend/Common/Services/IDirectoryService.cs
@@ -1,4 +1,6 @@
 using DTO.Directories;
+using DTO.Files;
+using Models.Enumerators;
 using Directory = Models.Directory;
 using File = Models.File;
 namespace Common.Services;
@@ -11,20 +13,32 @@ public interface IDirectoryService
     Task<Directory> GetDirectoryByIdAsync(Guid id, Guid userId, CancellationToken ct = default);
     Task<DirectorySummaryDto> GetDirectoryDtoByIdAsync(Guid id, Guid userId, CancellationToken ct = default);
 
+    Task<PaginatedResult<DirectorySummaryDto>> FindDirectoryAsync(Guid userId, DirectorySearchQuery query,
+        CancellationToken ct = default);
     Task<DirectoryDto> GetDirectoryWithDetailsAsync(Guid id, Guid userId, CancellationToken ct = default);
     
     Task<IEnumerable<DirectorySummaryDto>> GetUserDirectoriesAsync(Guid userId, Guid? parentId = null, 
         CancellationToken ct = default);
     
-    Task<RootContentSummaryDto> GetRootDirectoriesAsync(Guid userId, CancellationToken ct = default);
-    
     Task<IEnumerable<DirectorySummaryDto>> GetSubDirectoriesAsync(Guid directoryId, Guid userId,
+        CancellationToken ct = default);
+
+    Task<PaginatedResult<DirectorySummaryDto>> GetPaginatedDirectories(Guid id, Guid userId,
+        int currentPage = 1,
+        int pageSize = 25,
+        SortDirection sortDirection = SortDirection.Asc,
+        DirectorySortBy sortBy = DirectorySortBy.Name,
+        CancellationToken ct = default);
+    Task<PaginatedResult<DirectorySummaryDto>> GetRootDirectoriesAsync(
+        Guid ownerId,
+        int page = 1,
+        int pageSize = 25,
         CancellationToken ct = default);
     
     Task<IEnumerable<File>> GetDirectoryFilesAsync(Guid directoryId, Guid userId, 
         bool includeSubdirectories = false, CancellationToken ct = default);
     
-    Task<string> GetDirectoryPathAsync(Guid directoryId, Guid userId, CancellationToken ct = default);
+    Task<List<PathPartDto>> GetDirectoryPathAsync(Guid directoryId, Guid userId, CancellationToken ct = default);
     
     Task<DirectoryDto> UpdateDirectoryAsync(Guid id, string name, Guid userId, 
         CancellationToken ct = default);
@@ -36,6 +50,6 @@ public interface IDirectoryService
         CancellationToken ct = default);
     
     Task<bool> DirectoryExistsAsync(Guid id, CancellationToken ct = default);
-    
+    Task<bool> DirectoryExistsWithOwnershipAsync(Guid id, Guid ownerId, CancellationToken ct = default);
     Task<bool> HasAccessToDirectoryAsync(Guid directoryId, Guid userId, CancellationToken ct = default);
 }
