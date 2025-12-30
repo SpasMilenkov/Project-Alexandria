22c9f0f22cfcd89a2913b2fd57a9c55fcaa72cd6
Spas Milenkov <spasmilenkov123@gmail.com>
2025-12-30 18:12:47 +0200

    feat: add views to support onboarding flow

---
+++ Frontend/src/views/onboarding/SetupView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="flex flex-col gap-4 h-full">
+    <h1>This is just a placeholder, configurations are coming later!</h1>
+    <SimpleSetup />
+    <UButton
+      block
+      size="md"
+      color="primary"
+      variant="solid"
+      @click="router.push('/dashboard')"
+      >Finish setup!</UButton
+    >
+  </div>
+</template>
+<script setup lang="ts">
+import router from "@/router";
+import SimpleSetup from "@/components/setup/SimpleSetup,.vue";
+</script>
+<style scoped></style>
