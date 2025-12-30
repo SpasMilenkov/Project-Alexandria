1e34af725d32bbb40882bf39e21d67d5ea7d65fb
Spas Milenkov <spasmilenkov123@gmail.com>
2025-12-30 18:14:57 +0200

    feat: add logo component

---
+++ Frontend/src/components/LogoComponent.vue
@@ -0,0 +1,15 @@
+<template>
+  <router-link class="flex items-center gap-2" to="/">
+    <div class="bg-primary p-2 rounded-lg">
+      <Icon icon="heroicons:book-open" class="w-6 h-6 text-white" />
+    </div>
+
+    <span class="font-semibold text-lg">Alexandria</span>
+  </router-link>
+</template>
+
+<script setup lang="ts">
+import { Icon } from "@iconify/vue";
+</script>
+
+<style scoped></style>
