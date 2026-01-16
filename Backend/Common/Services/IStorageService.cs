4fcbf594f2abb29cc4a2dc358f6baeb33ea509b3
Spas Milenkov <spasmilenkov123@gmail.com>
2026-01-16 20:51:38 +0200

    chore: clean up unused methods

--- Backend/Common/Services/IStorageService.cs
+++ Backend/Common/Services/IStorageService.cs
@@ -1,4 +1,3 @@
-using DTO;
 using DTO.Files;
 using Models.Enumerators;
 using File = Models.File;
@@ -31,37 +30,29 @@ public interface IStorageService
         string? originalFileName = null,
         CancellationToken ct = default);
 
-    // Task UploadThumbnailAsync(
-    //     string objectName,
-    //     Stream fileStream,
-    //     long contentLength = -1,
-    //     CancellationToken ct = default);
-
     Task UploadMediaData(Stream previewStream, Stream thumbnailStream, string objectName, Guid fileId,
         MediaMetadata metadataDto,
         CancellationToken ct = default);
+
     // File Download
     Task<Stream> DownloadFile(Guid fileId, Guid ownerId, CancellationToken ct);
     Task<Stream> DownloadStreamableFile(Guid fileId, Guid userId, CancellationToken ct);
+
     Task StreamFile(
         string fileId,
         Stream destination,
         CancellationToken ct);
+
     Task<PreviewResultDto?> GetCachedPreview(Guid id, CancellationToken ct);
 
-    public string GetPreviewPresignedUrl(string objectKey, TimeSpan expiry);
-    //TODO: Determine if this is worth keeping
-    // Task<FileResult> GetFileById(Guid id, CancellationToken ct);
-
     FileCategory CategorizeFile(string mimeType);
+
     // File Metadata Operations
     Task<File?> GetFileMetadata(Guid fileId, CancellationToken ct = default);
     Task<IEnumerable<File>> GetFilesByMimeType(string mimeType, CancellationToken ct = default);
-    Task<IEnumerable<File>> GetAllFiles(CancellationToken ct = default);
-    Task<int> GetFileCount(string? mimeTypeFilter = null, CancellationToken ct = default);
 
     // File Operations
-    Task DeleteFile(Guid fileId, Guid userId, CancellationToken ct, bool hardDelete = false);
+    Task DeleteFiles(Guid[] fileIds, Guid userId, bool hardDelete = false, CancellationToken ct = default);
 
     Task<File> UpdateFileMetadata(
         Guid fileId,
@@ -71,13 +62,11 @@ public interface IStorageService
         CancellationToken ct = default);
 
     // Storage Management
-    Task EnsureBucketExistsAsync(string bucketName, CancellationToken ct);
     Task<VersionInfo> GetVersionInfo(string bucketName, string objectName, CancellationToken ct);
-    
-    // File Data Management
-    Task<FileSummary?> GetFileSummary(Guid fieldId, CancellationToken ct = default);
 
-    Task<PaginatedResult<FileResult>> GetRootFilesAsync(Guid ownerId, int page = 1, int pageSize = 25, SortBy sortBy = SortBy.Name, SortDirection sortDirection = SortDirection.Asc,
+    // File Data Management
+    Task<PaginatedResult<FileResult>> GetRootFilesAsync(Guid ownerId, int page = 1, int pageSize = 25,
+        SortBy sortBy = SortBy.Name, SortDirection sortDirection = SortDirection.Asc,
         CancellationToken ct = default);
 
     Task<PaginatedResult<FileResult>> GetFilesByDirectoryId(Guid directoryId,
@@ -87,4 +76,8 @@ public interface IStorageService
         SortBy sortBy = SortBy.Name,
         SortDirection sortDirection = SortDirection.Asc,
         CancellationToken ct = default);
+
+    Task MoveFilesAsync(Guid[] fileIds, Guid? destinationId, Guid userId, CancellationToken ct = default);
+
+    Task CopyFilesAsync(Guid[] fileIds, Guid destinationId, Guid userId, CancellationToken ct = default);
 }
