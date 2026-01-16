a18379d904e9005a23119019b9cd904ee8493f4f
Spas Milenkov <spasmilenkov123@gmail.com>
2026-01-16 20:52:37 +0200

    feat: add copy files endpoint

---
+++ Backend/API/Features/Storage/Files/CopyFiles/CopyFilesEndpoint.cs
@@ -0,0 +1,30 @@
+using System.Security.Claims;
+using Common.Services;
+using FastEndpoints;
+
+namespace API.Features.Storage.Files.CopyFiles;
+
+sealed class CopyFilesRequest
+{
+    public Guid[] FileIds { get; set; }
+    public Guid DestinationId { get; set; }
+}
+
+sealed class CopyDirectoryEndpoint(IStorageService storageService) : Endpoint<CopyFilesRequest>
+{
+    public override void Configure()
+    {
+        Post("/files/copy");
+    }
+
+    public override async Task HandleAsync(CopyFilesRequest req, CancellationToken ct)
+    {
+        var userIdString = User.FindFirst(ClaimTypes.NameIdentifier)?.Value
+                           ?? User.FindFirst("sub")?.Value
+                           ?? throw new UnauthorizedAccessException("User ID not found in token");
+        var userId = Guid.Parse(userIdString);
+
+        await storageService.CopyFilesAsync(req.FileIds, req.DestinationId, userId, ct);
+        await Send.OkAsync(cancellation: ct);
+    }
+}
