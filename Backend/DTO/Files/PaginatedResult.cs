69da0b637f8f9fabf44635a48fe2eebb6c1b914f
Spas Milenkov <spasmilenkov123@gmail.com>
2026-01-16 20:48:29 +0200

    fix: set default current page to 1 to fix issue with 0 starting page

--- Backend/DTO/Files/PaginatedResult.cs
+++ Backend/DTO/Files/PaginatedResult.cs
@@ -7,6 +7,6 @@ public class PaginatedResult<T>
     public int PageSize { get; set; }
     public int TotalCount { get; set; }
     public int TotalPages { get; set; }
-    public bool HasPrevious => CurrentPage > 0;
+    public bool HasPrevious => CurrentPage > 1;
     public bool HasNext => CurrentPage < TotalPages - 1;
 }
