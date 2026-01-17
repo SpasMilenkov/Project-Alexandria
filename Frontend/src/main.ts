290b3801d4990d49c02033ec37f604a9e0a2e19f
Spas Milenkov <spasmilenkov123@gmail.com>
2026-01-17 22:04:35 +0200

    feat: add pinia colada to app config

--- Frontend/src/main.ts
+++ Frontend/src/main.ts
@@ -3,17 +3,31 @@ import "./assets/main.css";
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 import ui from "@nuxt/ui/vue-plugin";
-import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
+import { PiniaColada } from "@pinia/colada";
 
 import App from "./App.vue";
 import router from "./router";
 
 const app = createApp(App);
 
-const pinia =createPinia() 
-pinia.use(piniaPluginPersistedstate)
+const pinia = createPinia();
+pinia.use(piniaPluginPersistedstate);
 
 app.use(pinia);
+app.use(PiniaColada, {
+  queryOptions: {
+    // change the stale time for all queries to 0ms
+    staleTime: 0,
+  },
+  mutationOptions: {
+    // add global mutation options here
+  },
+  plugins: [
+    // add Pinia Colada plugins here
+  ],
+});
+
 app.use(router);
 app.use(ui);
 
