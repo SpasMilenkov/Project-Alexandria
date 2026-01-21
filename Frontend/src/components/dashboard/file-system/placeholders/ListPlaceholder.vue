aaeda4ebf9a6a6e967204b2f4f3ccf00579c884e
Spas Milenkov <spasmilenkov123@gmail.com>
2026-01-21 19:20:21 +0200

    feat: add placeholder components to handle loading state

---
+++ Frontend/src/components/dashboard/file-system/placeholders/ListPlaceholder.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="flex items-center gap-4 p-4 flex-wrap">
+    <div v-for="i in 12" :key="i" class="flex items-center gap-4">
+      <USkeleton class="h-12 w-12 rounded-md" />
+      <USkeleton class="h-4 w-screen" />
+    </div>
+  </div>
+</template>
+<script setup lang="ts"></script>
+<style scoped></style>
