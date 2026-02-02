a564230c3aace8e87994b23217fbac311dcc2c52
Spas Milenkov <spasmilenkov123@gmail.com>
2026-02-02 12:34:41 +0200

    feat: add extra tag properties migration

---
+++ Backend/Data/Migrations/20260122162738_AddAdditionalTagProperties.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAdditionalTagProperties : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Color",
+                table: "Tags",
+                type: "varchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Tags",
+                type: "varchar(255)",
+                maxLength: 255,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Icon",
+                table: "Tags",
+                type: "varchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Color",
+                table: "Tags");
+
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Tags");
+
+            migrationBuilder.DropColumn(
+                name: "Icon",
+                table: "Tags");
+        }
+    }
+}
