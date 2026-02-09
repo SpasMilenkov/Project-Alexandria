367f5b025441dfc0660b491f643c315064d8854f
Spas Milenkov <spasmilenkov123@gmail.com>
2026-02-09 15:18:32 +0200

    feat: add promotion tracking, add relation to successfull upload

--- Backend/Models/ContentObject.cs
+++ Backend/Models/ContentObject.cs
@@ -6,9 +6,16 @@ public class ContentObject : IBase
     public required byte[] Hash { get; set; } = [];
     public required string StorageKey { get; set; }
     public int RefCount { get; set; }
+    public bool IsPromoted { get; set; } = false;
+    public DateTime? PromotedAt { get; set; }
+    public int PromotionAttempts { get; set; } = 0;
+    public DateTime? LastPromotionAttemptAt { get; set; }
     public DateTime? OrphanedAt { get; set; }
     public DateTime CreatedAt { get; set; }
     public DateTime? UpdatedAt { get; set; }
     public DateTime? DeletedAt { get; set; }
     public Guid? UpdatedBy { get; set; }
+
+    public Guid? UploadId { get; set; }
+    public Upload? Upload { get; set; }
 }
