674b590d60e0e9c031946bfc492117ff92c06cdb
Spas Milenkov <spasmilenkov123@gmail.com>
2026-02-13 20:17:54 +0200

    feat: add file search query

--- Frontend/src/queries/files.ts
+++ Frontend/src/queries/files.ts
@@ -1,4 +1,5 @@
 import { fileApi } from "@/api/file";
+import type { FileSearchQuery } from "@/schemas/search";
 import type { PaginationParams } from "@/types/pagination-params";
 import { defineQueryOptions } from "@pinia/colada";
 
@@ -23,6 +24,18 @@ export const FILES_QUERY_KEYS = {
     params.sortDirection,
     params.orderBy,
   ],
+  searchFiles: (query: FileSearchQuery) => [
+    ...FILES_QUERY_KEYS.root,
+    "search-files",
+    query.nameContains ?? null,
+    query.isDeleted,
+    query.isShared,
+    query.isStarred,
+    query.currentPage,
+    query.sortDirection,
+    query.sortBy,
+    query.pageSize,
+  ],
 };
 
 export const subFiles = defineQueryOptions(
@@ -36,7 +49,7 @@ export const subFiles = defineQueryOptions(
 export const getPreview = defineQueryOptions((id: string) => ({
   key: FILES_QUERY_KEYS.signedUrl(id),
   query: () => fileApi.getPreview(id),
-  refetchOnMount: false,
+  refetchOnMount: true,
   staleTime: 60000,
 }));
 
@@ -44,4 +57,9 @@ export const rootFiles = defineQueryOptions((params: PaginationParams) => ({
   key: FILES_QUERY_KEYS.rootFiles(params),
   query: () => fileApi.getRootFiles(params),
   placeholderData: (prev) => prev,
+}));
+
+export const searchFile = defineQueryOptions((query: FileSearchQuery) => ({
+  key: FILES_QUERY_KEYS.searchFiles(query),
+  query: () => fileApi.searchFiles(query),
 }));
