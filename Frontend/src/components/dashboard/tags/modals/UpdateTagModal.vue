f9a1b4e972ee5bd044818653b7103d7a5fe9ae9f
Spas Milenkov <spasmilenkov123@gmail.com>
2026-02-18 14:17:12 +0200

    fix: preview card overflowing on mobile

--- Frontend/src/components/dashboard/tags/modals/UpdateTagModal.vue
+++ Frontend/src/components/dashboard/tags/modals/UpdateTagModal.vue
@@ -67,7 +67,7 @@
 
         <UFormField label="Preview">
           <div
-            class="flex flex-wrap items-center justify-center gap-4 p-4 border border-primary/60 rounded-lg h-64"
+            class="flex flex-wrap items-center justify-center gap-4 p-4 border border-primary/60 rounded-lg h-full"
           >
             <div class="flex flex-col-reverse gap-3">
               <TagCard
