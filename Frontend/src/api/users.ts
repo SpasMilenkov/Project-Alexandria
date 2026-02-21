8a0fa1b063dd8c8108ae1c7bd1e204e0fb37df04
Spas Milenkov <spasmilenkov123@gmail.com>
2026-02-21 22:36:34 +0200

    feat: add user api

---
+++ Frontend/src/api/users.ts
@@ -0,0 +1,71 @@
+import type { UpdateUserDto, UserDetailsDto, UserQueryDto } from "@/types/user";
+import apiClient from "./client";
+
+import type { PaginatedResponse } from "./directory";
+
+export const userApi = {
+  getUsers: async (
+    query: UserQueryDto,
+  ): Promise<PaginatedResponse<UserDetailsDto>> => {
+    const result = await apiClient.get<PaginatedResponse<UserDetailsDto>>(
+      "/users",
+      {
+        params: query,
+      },
+    );
+    return result.data;
+  },
+
+  updateUser: async (
+    userId: string,
+    payload: UpdateUserDto,
+  ): Promise<UserDetailsDto> => {
+    const result = await apiClient.patch<UserDetailsDto>("/users", {
+      userId,
+      payload,
+    });
+    return result.data;
+  },
+
+  restrictUser: async (
+    userId: string,
+    lockoutEndDate: string,
+  ): Promise<void> => {
+    await apiClient.patch("/users/restrict", {
+      userId,
+      lockoutEndDate,
+    });
+  },
+
+  getFileCountPerUser: async (
+    userId: string,
+    deletedOnly: boolean,
+  ): Promise<number> => {
+    const result = await apiClient.get<number>("/users/file-count", {
+      params: {
+        userId,
+        deletedOnly,
+      },
+    });
+    return result.data;
+  },
+
+  getFileSizePerUser: async (
+    userId: string,
+    deletedOnly: boolean,
+  ): Promise<number> => {
+    const result = await apiClient.get<number>("/users/file-size", {
+      params: {
+        userId,
+        deletedOnly,
+      },
+    });
+    return result.data;
+  },
+
+  deleteUsers: async (userIds: string[]): Promise<void> => {
+    await apiClient.delete("/users", {
+      data: { userIds },
+    });
+  },
+};
