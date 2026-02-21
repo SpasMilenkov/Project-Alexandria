872b8127a0bdba5f7d42ff6842d565cf70516680
Spas Milenkov <spasmilenkov123@gmail.com>
2026-02-21 22:27:20 +0200

    feat: add local start time migrations

--- Backend/Data/Migrations/20260219132059_AddLockoutStartedToTheUser.Designer.cs
+++ Backend/Data/Migrations/20260219132059_AddLockoutStartedToTheUser.Designer.cs
@@ -4,6 +4,7 @@ using System.Numerics;
 using Data.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using NpgsqlTypes;
@@ -13,9 +14,11 @@ using NpgsqlTypes;
 namespace Data.Migrations
 {
     [DbContext(typeof(AlexandriaDbContext))]
-    partial class AlexandriaDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260219132059_AddLockoutStartedToTheUser")]
+    partial class AddLockoutStartedToTheUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
