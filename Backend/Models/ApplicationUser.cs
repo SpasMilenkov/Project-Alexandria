f395a755212c917957e2825562a105e6f4bb3aeb
Spas Milenkov <spasmilenkov123@gmail.com>
2026-02-21 22:27:06 +0200

    feat: add property for tracking lockout start time

--- Backend/Models/ApplicationUser.cs
+++ Backend/Models/ApplicationUser.cs
@@ -2,9 +2,10 @@ using Microsoft.AspNetCore.Identity;
 
 namespace Models;
 
-public class ApplicationUser: IdentityUser<Guid>, IBase
+public class ApplicationUser : IdentityUser<Guid>, IBase
 {
     public required string Name { get; set; }
+    public DateTime? LockoutStartedAt { get; set; }
     public DateTime CreatedAt { get; set; }
     public DateTime? UpdatedAt { get; set; }
     public DateTime? DeletedAt { get; set; }
