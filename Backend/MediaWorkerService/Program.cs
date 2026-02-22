4d6371b7c3da1aa5332820d1606de74928bdc69d
Spas Milenkov <spasmilenkov123@gmail.com>
2026-02-22 18:51:23 +0200

    refactor: change media worker to use the worker database extension

--- Backend/MediaWorkerService/Program.cs
+++ Backend/MediaWorkerService/Program.cs
@@ -9,7 +9,7 @@ Console.WriteLine($"Environment: {builder.Environment.EnvironmentName}");
 Console.WriteLine($"RabbitMQ Host: {config["RabbitMQ:HostName"]}");
 Console.WriteLine($"RabbitMQ Port: {config["RabbitMQ:Port"]}");
 builder.Services
-    .AddDatabase(builder.Configuration)
+    .AddWorkerDatabase(builder.Configuration)
     .AddS3Storage(builder.Configuration)
     .AddRabbitMqConsumer(builder.Configuration)
     .AddPreviewWorkerServices()
