c04a5f99da363747e7844c6cb837f9c23b8c180e
Spas Milenkov <spasmilenkov123@gmail.com>
2026-02-23 18:33:40 +0200

    feat: convert workert worker user to use the webapplication builder and expose health endpoint

--- Backend/DocumentWorker.Service/Program.cs
+++ Backend/DocumentWorker.Service/Program.cs
@@ -2,15 +2,21 @@ using DocumentWorker.Service;
 using DocumentWorker.Service.Extensions;
 using Infrastructure;
 using Infrastructure.DocumentWorker;
+using Microsoft.AspNetCore.Builder;
 
-var builder = Host.CreateApplicationBuilder(args);
+var builder = WebApplication.CreateBuilder(args);
+
 builder.Services
     .AddWorkerDatabase(builder.Configuration)
     .AddS3Storage(builder.Configuration)
     .AddRabbitMqConsumer(builder.Configuration)
-    .AddSWorkerServices();
+    .AddSWorkerServices()
+    .AddHealthChecks();
 
 builder.Services.AddHostedService<Worker>();
 
 var host = builder.Build();
+
+host.MapHealthChecks("/health");
+
 host.Run();
