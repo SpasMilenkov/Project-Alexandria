74f61175be7c538295941feb7214241eba8fcfaf
Spas Milenkov <spasmilenkov123@gmail.com>
2026-02-28 21:34:11 +0200

    fix: missing service registration when initializing app component test

--- Frontend/src/__tests__/App.spec.ts
+++ Frontend/src/__tests__/App.spec.ts
@@ -1,11 +1,24 @@
 import { mount } from "@vue/test-utils";
+import { createPinia } from "pinia";
 import { describe, expect, it } from "vitest";
+import { createMemoryHistory, createRouter } from "vue-router";
 
 import App from "../App.vue";
 
+const router = createRouter({
+  history: createMemoryHistory(),
+  routes: [{ component: { template: "<div>You did it!</div>" }, path: "/" }],
+});
+
 describe("App", () => {
-  it("mounts renders properly", () => {
-    const wrapper = mount(App);
+  it("mounts renders properly", async () => {
+    const wrapper = mount(App, {
+      global: {
+        plugins: [createPinia(), router],
+      },
+    });
+
+    await router.isReady();
     expect(wrapper.text()).toContain("You did it!");
   });
 });
