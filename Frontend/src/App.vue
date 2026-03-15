13bff5285895549cd3d792f3a1568e9986305111
Spas Milenkov <spasmilenkov123@gmail.com>
2026-03-15 18:14:29 +0200

    feat: add onboarding layout

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -2,14 +2,17 @@
 import { useRoute } from "vue-router";
 import DefaultLayout from "@/layouts/DefaultLayout.vue";
 import DashboardLayout from "@/layouts/DashboardLayout.vue";
+import OnboardingLayout from "@/layouts/OnboardingLayout.vue";
 import { PiniaColadaDevtools } from "@pinia/colada-devtools";
 import { useTheme } from "./composables/useTheme";
-const route = useRoute();
 
+const route = useRoute();
 useTheme();
+
 const layouts = {
   dashboard: DashboardLayout,
   default: DefaultLayout,
+  onboarding: OnboardingLayout,
 };
 </script>
 
