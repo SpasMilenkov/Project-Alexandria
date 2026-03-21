4ff3870866b321c0aee2d633b37adfcf37047c10
Spas Milenkov <spasmilenkov123@gmail.com>
2026-03-21 15:00:12 +0200

    feat: add eventcode config, remove old description from config, add description fallback config

--- Backend/Data/Configurations/AuditLogConfiguration.cs
+++ Backend/Data/Configurations/AuditLogConfiguration.cs
@@ -9,29 +9,30 @@ public class AuditLogConfiguration : IEntityTypeConfiguration<AuditLog>
     public void Configure(EntityTypeBuilder<AuditLog> builder)
     {
         builder.ToTable("AuditLogs");
-
         builder.HasKey(a => a.Id);
 
-        // Convert OperationType enum to string in database
         builder.Property(a => a.OperationType)
             .HasConversion<string>()
             .HasMaxLength(ValidationConstants.StringLengths.ShortString)
             .IsRequired();
 
-        // Convert EntityType enum to string in database
         builder.Property(a => a.EntityType)
             .HasConversion<string>()
             .HasMaxLength(ValidationConstants.StringLengths.ShortString)
             .IsRequired();
 
-        // Store MetadataJson as JSON column type
-        builder.Property(a => a.MetadataJson)
-            .HasColumnType("json")
+        builder.Property(a => a.EventCode)
+            .HasConversion<string>()
+            .HasMaxLength(ValidationConstants.StringLengths.ShortString)
+            .IsRequired();
+
+        builder.Property(a => a.FallbackDescription)
+            .HasMaxLength(ValidationConstants.StringLengths.LongString)
             .IsRequired(false);
 
-        builder.Property(a => a.Description)
-            .HasMaxLength(ValidationConstants.StringLengths.LongString)
-            .IsRequired();
+        builder.Property(a => a.MetadataJson)
+            .HasColumnType("jsonb")
+            .IsRequired(false);
 
         builder.Property(a => a.Source)
             .HasConversion<string>()
@@ -44,11 +45,12 @@ public class AuditLogConfiguration : IEntityTypeConfiguration<AuditLog>
         builder.Property(a => a.Timestamp)
             .IsRequired();
 
-        builder.Property(a => a.UserId).IsRequired(false);
+        builder.Property(a => a.UserId)
+            .IsRequired(false);
 
-        // Optional: Add index for common queries
         builder.HasIndex(a => a.Timestamp);
         builder.HasIndex(a => a.UserId);
+        builder.HasIndex(a => a.EventCode);
         builder.HasIndex(a => new { a.EntityType, a.EntityId });
     }
 }
