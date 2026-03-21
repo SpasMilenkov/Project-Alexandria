bdc1c9191afaed3fc7ec5aca35b579149edefe3d
Spas Milenkov <spasmilenkov123@gmail.com>
2026-03-21 15:26:49 +0200

    feat: set nuxt UI warning color to orange for better clarity in different themes

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -15,6 +15,7 @@ export default defineConfig({
           neutral: "neutral",
           primary: "orange",
           secondary: "orange-700",
+          warning: "orange",
         },
         container: {
           base: "w-full max-w-(--ui-container) mx-auto px-4 sm:px-6 lg:px-8",
