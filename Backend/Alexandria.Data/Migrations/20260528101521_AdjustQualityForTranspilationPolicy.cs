af210b23491b57dcc95b713675db6b30ea1d440f
Spas Milenkov <spasmilenkov123@gmail.com>
2026-06-05 19:11:47 +0300

    feat(migrations): add rungs for audio and video quality

---
+++ Backend/Alexandria.Data/Migrations/20260528101521_AdjustQualityForTranspilationPolicy.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Alexandria.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdjustQualityForTranspilationPolicy : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int[]>(
+                name: "AudioRungs",
+                table: "TranspilationJobs",
+                type: "integer[]",
+                nullable: false,
+                defaultValue: new int[0]);
+
+            migrationBuilder.AddColumn<int[]>(
+                name: "VideoRungs",
+                table: "TranspilationJobs",
+                type: "integer[]",
+                nullable: false,
+                defaultValue: new int[0]);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AudioRungs",
+                table: "TranspilationJobs");
+
+            migrationBuilder.DropColumn(
+                name: "VideoRungs",
+                table: "TranspilationJobs");
+        }
+    }
+}
