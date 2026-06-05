d6e2afc22e9493d8fc1d960ccf5f77bc0c4895ea
Spas Milenkov <spasmilenkov123@gmail.com>
2026-06-05 19:20:51 +0300

    feat: add playlist DTOs

--- Backend/Alexandria.Dto/Files/Streaming/Playlist/PlaylistItemDto.cs
+++ Backend/Alexandria.Dto/Files/Streaming/Playlist/PlaylistItemDto.cs
@@ -1,6 +1,13 @@
 namespace Alexandria.Dto.Files.Streaming.Playlist;
 
-public class PlaylistItemDto
+public record PlaylistItemDto
 {
-    
+    public Guid Id { get; init; }
+    public int Position { get; init; }
+    public Guid TranspilationJobId { get; init; }
+    public string FileName { get; init; } = null!;
+    public string MimeType { get; init; } = null!;
+    public string? SegmentPrefix { get; init; }
+    public IReadOnlyList<StreamingRepresentationDto> Representations { get; init; } = [];
+    public DateTime CreatedAt { get; init; }
 }
