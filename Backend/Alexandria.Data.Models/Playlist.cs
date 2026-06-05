1b39a678327ea7c314060dae27ad85be64192f17
Spas Milenkov <spasmilenkov123@gmail.com>
2026-06-05 19:24:38 +0300

    feat: add playlist and playlist item models

--- Backend/Alexandria.Data.Models/Playlist.cs
+++ Backend/Alexandria.Data.Models/Playlist.cs
@@ -1,6 +1,19 @@
 namespace Alexandria.Data.Models;
 
-public class Playlist
+public class Playlist : IBase
 {
-    
+    public Guid Id { get; set; }
+    public required string Name { get; set; }
+    public string? Description { get; set; }
+    public bool HasCover { get; set; } = false;
+    public string? AmbientTheme { get; set; }
+
+    public Guid OwnerId { get; set; }
+    public ApplicationUser? Owner { get; set; }
+    public ICollection<PlaylistItem>? PlaylistItems { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+    public DateTime? UpdatedAt { get; set; }
+    public DateTime? DeletedAt { get; set; }
+    public Guid? UpdatedBy { get; set; }
 }
--- Backend/Alexandria.Data.Models/PlaylistItem.cs
+++ Backend/Alexandria.Data.Models/PlaylistItem.cs
@@ -1,6 +1,18 @@
 namespace Alexandria.Data.Models;
 
-public class PlaylistItem
+public class PlaylistItem : IBase
 {
-    
+    public Guid Id { get; set; }
+    public int Position { get; set; }
+
+    public Guid PlaylistId { get; set; }
+    public Playlist? Playlist { get; set; }
+
+    public Guid TranspilationJobId { get; set; }
+    public TranspilationJob? TranspilationJob { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+    public DateTime? UpdatedAt { get; set; }
+    public DateTime? DeletedAt { get; set; }
+    public Guid? UpdatedBy { get; set; }
 }
