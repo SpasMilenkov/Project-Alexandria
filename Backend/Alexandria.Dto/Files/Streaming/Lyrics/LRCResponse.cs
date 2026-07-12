2f33c7c0d978fd363f4c8bb5676f267728b25181
Spas Milenkov <spasmilenkov123@gmail.com>
2026-07-12 21:32:02 +0300

    feat: add lyrics related DTOs

--- Backend/Alexandria.Dto/Files/Streaming/Lyrics/LRCResponse.cs
+++ Backend/Alexandria.Dto/Files/Streaming/Lyrics/LRCResponse.cs
@@ -1,6 +1,19 @@
 namespace Alexandria.Dto.Files.Streaming.Lyrics;
 
-public class LRCResponse
-{
-    
-}
+/// <summary>
+/// Response that is coming from LRCLIB. Comes with 2 formats of lyrics
+/// [mm:ss.xx] timestamps for the synced and plain lyrics
+/// When the music piece is instrumental both should be null
+/// and the instrumental bool should be set to true
+/// </summary>
+public sealed record LrcLibResponse(
+    int Id,
+    string TrackName,
+    string ArtistName,
+    string? AlbumName,
+    double Duration,
+    bool Instrumental,
+    string? PlainLyrics,
+    string? SyncedLyrics,
+    string? LyricsFile = null
+);
--- Backend/Alexandria.Dto/Files/Streaming/Lyrics/LyricsResult.cs
+++ Backend/Alexandria.Dto/Files/Streaming/Lyrics/LyricsResult.cs
@@ -1,6 +1,12 @@
+using Alexandria.Data.Models.Enumerators;
+
 namespace Alexandria.Dto.Files.Streaming.Lyrics;
 
-public class LyricsResult
-{
-    
-}
+public record LyricsResult(
+    string? PlainLyrics,
+    string? SyncedLyrics,
+    string? ProviderTrackId,
+    bool Instrumental,
+    LyricsProvider Provider,
+    decimal ConfidenceScore
+);
