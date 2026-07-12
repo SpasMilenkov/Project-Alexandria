700c0eab784595debf9118309e5fd8f8298ea9df
Spas Milenkov <spasmilenkov123@gmail.com>
2026-07-12 21:29:17 +0300

    feat: add lyrics configuration

---
+++ Backend/Alexandria.Data/Configurations/TrackLyricsConfiguration.cs
@@ -0,0 +1,43 @@
+using Alexandria.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Alexandria.Data.Configurations;
+
+public class TrackLyricsConfiguration: IEntityTypeConfiguration<TrackLyrics>
+{
+
+    public void Configure(EntityTypeBuilder<TrackLyrics> builder)
+    {
+        builder.Property(e => e.PlainLyrics)
+            .HasColumnType("text")
+            .IsRequired(false);
+        
+        builder.Property(e => e.SyncedLyrics)
+            .HasColumnType("text")
+            .IsRequired(false);
+        
+        builder.Property(e => e.CreatedAt)
+            .HasColumnType("timestamp with time zone")
+            .IsRequired();
+
+        builder.Property(e => e.UpdatedAt)
+            .HasColumnType("timestamp with time zone")
+            .IsRequired(false);
+
+        builder.Property(e => e.DeletedAt)
+            .HasColumnType("timestamp with time zone")
+            .IsRequired(false);
+
+        builder.HasOne(e => e.TranspilationJob)
+            .WithOne(e => e.TrackLyrics);
+        
+        builder.HasIndex(e => e.Id);
+        builder.HasIndex(e => e.Status);
+        builder.HasIndex(e => e.TranspilationJobId)
+            .IsUnique()
+            .HasFilter("\"DeletedAt\" IS NULL");
+        
+        builder.ToTable("TrackLyrics");
+    }
+}
