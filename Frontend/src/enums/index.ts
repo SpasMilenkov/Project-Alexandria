1e56d53196648f84d2696355f2895dda3f9f3873
Spas Milenkov <spasmilenkov123@gmail.com>
2026-07-12 21:41:01 +0300

    chore: export new enums

--- Frontend/src/enums/index.ts
+++ Frontend/src/enums/index.ts
@@ -3,6 +3,8 @@ import type { SortDirection } from "./SortDirection";
 import type { UserRole } from "./UserRole";
 
 import { OnboardingStep } from "./OnboardingStep";
+import { LyricsProvider } from "./lyrics-provider";
+import { LyricsStatus } from "./lyrics-status";
 
 export type { SortBy, SortDirection, UserRole };
-export { OnboardingStep };
+export { OnboardingStep, LyricsProvider, LyricsStatus };
