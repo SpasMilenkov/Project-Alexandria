01443243a866602b0739d7f2b33f7dc37179b7d3
Spas Milenkov <spasmilenkov123@gmail.com>
2026-07-12 21:31:43 +0300

    feat: add lyrics not found exception

--- Backend/Alexandria.Common/Exceptions/Streaming/Lyrics/LyricsNotFoundException.cs
+++ Backend/Alexandria.Common/Exceptions/Streaming/Lyrics/LyricsNotFoundException.cs
@@ -1,6 +1,3 @@
 namespace Alexandria.Common.Exceptions.Streaming.Lyrics;
 
-public class LyricsNotFoundException
-{
-    
-}
+public class LyricsNotFoundException(Guid id) : Exception($"Lyrics for song with {id} not found");
