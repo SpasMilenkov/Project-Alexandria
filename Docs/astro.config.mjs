4d3bb32cca9e12bc82dde53ec33c35c9b80b50b2
Spas Milenkov <spasmilenkov123@gmail.com>
2026-07-12 21:42:40 +0300

    feat(docs): add mermaid rendering

--- Docs/astro.config.mjs
+++ Docs/astro.config.mjs
@@ -1,10 +1,15 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
+import mermaid from 'astro-mermaid';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
+    mermaid({
+      theme: 'forest',
+      autoTheme: true
+    }),
     starlight({
       title: "Alexandria",
       customCss: ["./src/styles/custom.css"],
@@ -33,6 +38,7 @@ export default defineConfig({
               label: "Development Setup",
               slug: "guides/set-up-development-environment",
             },
+
           ],
         },
         {
