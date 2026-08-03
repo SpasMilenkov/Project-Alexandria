97a411eddf3fc80bab2c252ee17df695380fd244
Spas Milenkov <spasmilenkov123@gmail.com>
2026-08-03 12:14:10 +0300

    feat: separate media metadata worker and lyrics worker as two separate parts of the script

--- run-dev.sh
+++ run-dev.sh
@@ -43,6 +43,7 @@ doc_worker_profile="DocumentWorker ($S3_PROVIDER)"
 media_worker_profile="MediaWorker ($S3_PROVIDER)"
 streaming_worker_profile="Alexandria.Workers.MediaTranspilation ($S3_PROVIDER)"
 media_metadata_worker_profile="Alexandria.Workers.MediaMetadata ($S3_PROVIDER)"
+lyrics_worker_profile="Alexandria.Workers.Lyrics ($S3_PROVIDER)"
 # ----------------------------
 # tmux setup
 # ----------------------------
@@ -73,10 +74,15 @@ sleep 5.0
 tmux send-keys -t "$SESSION_NAME:TranspilationWorker" \
   "cd Backend && dotnet run --project Alexandria.Workers.MediaTranspilation --launch-profile \"$streaming_worker_profile\"" C-m
 
-tmux new-window -t "$SESSION_NAME" -n "MediaMetadata" -c "$PWD"
+tmux new-window -t "$SESSION_NAME" -n "MediaMetadataWorker" -c "$PWD"
 sleep 5.0
-tmux send-keys -t "$SESSION_NAME:MediaMetadata" \
+tmux send-keys -t "$SESSION_NAME:MediaMetadataWorker" \
   "cd Backend && dotnet run --project Alexandria.Workers.MediaMetadata --launch-profile \"$media_metadata_worker_profile\"" C-m
+
+tmux new-window -t "$SESSION_NAME" -n "LyricsWorker" -c "$PWD"
+sleep 5.0
+tmux send-keys -t "$SESSION_NAME:LyricsWorker" \
+    "cd Backend && dotnet run --project Alexandria.Workers.Lyrics --launch-profile \"$lyrics_worker_profile\"" C-m
 
 tmux select-window -t "$SESSION_NAME:Frontend"
 tmux attach-session -t "$SESSION_NAME"
