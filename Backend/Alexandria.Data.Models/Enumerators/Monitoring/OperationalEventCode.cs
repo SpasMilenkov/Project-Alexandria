be8add5de4371ffbccf23573d6ed5b7819b9c398
Spas Milenkov <spasmilenkov123@gmail.com>
2026-08-27 14:30:54 +0300

    feat: add operational event enumerators

---
+++ Backend/Alexandria.Data.Models/Enumerators/Monitoring/OperationalEventCode.cs
@@ -0,0 +1,9 @@
+namespace Alexandria.Data.Models.Enumerators.Monitoring;
+
+public enum OperationalEventCode
+{
+    HealthcheckUnhealthy,
+    HealthcheckUnreachable,
+    ErrorRateThresholdExceeded,
+    UserReportedProblem,
+}
---
+++ Backend/Alexandria.Data.Models/Enumerators/Monitoring/OperationalEventStatus.cs
@@ -0,0 +1,7 @@
+namespace Alexandria.Data.Models.Enumerators.Monitoring;
+
+public enum OperationalEventStatus
+{
+    Active,
+    Resolved
+}
