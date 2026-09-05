483ca120c31aca451223c28d1fce30d2ef67fce1
Spas Milenkov <spasmilenkov123@gmail.com>
2026-09-05 18:01:17 +0300

    feat: add storage quota to user

--- Backend/Alexandria.Data.Models/ApplicationUser.cs
+++ Backend/Alexandria.Data.Models/ApplicationUser.cs
@@ -12,4 +12,5 @@ public class ApplicationUser : IdentityUser<Guid>, IBase
     public DateTime? UpdatedAt { get; set; }
     public DateTime? DeletedAt { get; set; }
     public Guid? UpdatedBy { get; set; }
+    public long StorageQuota { get; set; }
 }
