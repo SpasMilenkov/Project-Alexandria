70a46ecf471183e74fff9877d189e3ad667e02ad
Spas Milenkov <spasmilenkov123@gmail.com>
2026-09-15 20:40:22 +0300

    feat(frontend): add playlist grouping enums

---
+++ Frontend/src/enums/auto-group-kind.ts
@@ -0,0 +1,8 @@
+export enum AutoGroupKind {
+  Artist = 0,
+  Album = 1,
+  Tag = 2,
+  Genre = 3,
+  ParentGenre = 4,
+  Decade = 5,
+}
---
+++ Frontend/src/enums/playlist-sort.ts
@@ -0,0 +1,5 @@
+export enum PlaylistSort {
+  Newest = 0,
+  Name = 1,
+  TrackCount = 2,
+}
